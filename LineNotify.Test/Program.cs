36a334294d4db742ed2b952620c0a7b6dc83461a
Larry.Lai <Larry.Lai@portwell.com.tw>
2023-02-14 17:45:53 +0800

    init

---
+++ LineNotify.Test/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace LineNotify.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Msg = "Test OK";
+            try
+            {
+                {
+                    var token = "";
+                    var message = "測試訊息";
+					
+                    var api = new api();
+                    var Result = api.notify(token, "GetResult" + message);
+                    if (Result is null) throw new Exception("result is null");
+
+                    Console.WriteLine($"GetResult：status={Result.status};message={Result.message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Msg = $"FAIL：{ex.Message}";
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine(Msg);
+                Console.WriteLine();
+                Console.WriteLine("press any key to exit......");
+                Console.ReadKey();
+            }
+        }
+    }
+}
